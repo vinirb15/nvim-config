4e7610fe154ec6884ec0dd6189914f39126cf4ef
vbueno <viniciusbueno15@gmail.com>
2024-09-11 18:11:04 -0300

    [feat]: add mdreader.lua

---
+++ lua/plugins/mdreader.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'iamcco/markdown-preview.nvim',
+    build = function() vim.fn["mkdp#util#install"]() end,
+    ft = { "markdown" },
+    config = function()
+      vim.g.mkdp_auto_start = 1 -- Inicia automaticamente ao abrir um arquivo markdown
+    end
+  }
+}
+
