0644def54f38218a7fec304e874212e023818c8b
Vinícius Bueno <viniciusbueno15@gmail.com>
2024-09-30 17:40:25 -0300

    [fix]: leader button

--- lua/plugins/mapper.lua
+++ lua/plugins/mapper.lua
@@ -1,5 +1,5 @@
 return {
- vim.keymap.set('n', '<Space>W', ':noa w<CR>', { noremap = true, silent = true });
+ vim.keymap.set('n', '<leader>W', ':noa w<CR>', { noremap = true, silent = true });
  vim.keymap.set('n', '<leader>P', ':lua print(vim.fn.expand("%:p"))<CR>', { noremap = true, silent = true });
- vim.keymap.set('n', '<Space>V', ':w | !open %', { noremap = true, silent = true });
+ vim.keymap.set('n', '<leader>V', ':w | !open %', { noremap = true, silent = true });
 }
