41d4af6d045dad75522c3fd9114256291978cfd1
vbueno <viniciusbueno15@gmail.com>
2024-09-30 17:50:54 -0300

    [fix]: add commands via cmd

--- lua/plugins/mapper.lua
+++ lua/plugins/mapper.lua
@@ -1,6 +1,6 @@
 return {
- vim.keymap.set('n', '<leader>W', ':noa w<CR>', { noremap = true, silent = true });
- vim.keymap.set('n', '<leader>P', ':lua print(vim.fn.expand("%:p"))<CR>', { noremap = true, silent = true });
- vim.keymap.set('n', '<leader>V', ':w | !open %', { noremap = true, silent = true });
- vim.keymap.set('n', '<leader>T', ':tab split #', { noremap = true, silent = true });
+  vim.keymap.set('n', '<leader>W', function() vim.cmd('noa w') end, { noremap = true, silent = true }),
+  vim.keymap.set('n', '<leader>P', function() print(vim.fn.expand("%:p")) end, { noremap = true, silent = true }),
+  vim.keymap.set('n', '<leader>V', function() vim.cmd('w') vim.cmd('!open %') end, { noremap = true, silent = true }),
+  vim.keymap.set('n', '<leader>T', function() vim.cmd('tab split #') end, { noremap = true, silent = true }),
 }
