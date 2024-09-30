766e7f613c3ca55e4526e6cf8ecfade1d3e8938a
vbueno <viniciusbueno15@gmail.com>
2024-09-30 17:42:09 -0300

    [feat]: add closed buffer

--- lua/plugins/mapper.lua
+++ lua/plugins/mapper.lua
@@ -2,4 +2,5 @@ return {
  vim.keymap.set('n', '<leader>W', ':noa w<CR>', { noremap = true, silent = true });
  vim.keymap.set('n', '<leader>P', ':lua print(vim.fn.expand("%:p"))<CR>', { noremap = true, silent = true });
  vim.keymap.set('n', '<leader>V', ':w | !open %', { noremap = true, silent = true });
+ vim.keymap.set('n', '<leader>T', ':tab split #', { noremap = true, silent = true });
 }
