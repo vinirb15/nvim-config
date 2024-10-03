2ae4836a9a136f726d90d1230ac013a22ea31016
vbueno <viniciusbueno15@gmail.com>
2024-10-03 17:48:11 -0300

    [feat]: add git commandas

--- lua/plugins/mapper.lua
+++ lua/plugins/mapper.lua
@@ -3,4 +3,6 @@ return {
   vim.keymap.set('n', '<leader>P', function() print(vim.fn.expand("%:p")) end, { noremap = true, silent = true, desc = 'Print current file path' }),
   vim.keymap.set('n', '<leader>V', function() vim.cmd('w') vim.cmd('!open %') end, { noremap = true, silent = true, desc = 'Save and open in default application' }),
   vim.keymap.set('n', '<leader>T', function() vim.cmd('tab split #') end, { noremap = true, silent = true, desc = 'Split tab with alternate file' }),
+  vim.keymap.set('n', '<leader>ga', function() require('gitsigns').stage_buffer() end, { noremap = true, silent = true, desc = 'Git add current buffer' }),
+  vim.keymap.set('n', '<leader>gr', function() require('gitsigns').undo_stage_buffer() end, { noremap = true, silent = true, desc = 'Git unstage current buffer' })
 }
