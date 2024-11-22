526d1b156e487c69f3c0c5bfa8d8a99c2a294fe0
vbueno <viniciusbueno15@gmail.com>
2024-11-22 11:29:37 -0300

    [fix]: eslint rules

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,24 +10,46 @@ return {
     require("mason").setup({})
     require("mason-lspconfig").setup({
       ensure_installed = {
-        "tsserver",
-        -- "eslint",
+        -- "hadolint",
+        -- "prettier",
+        "eslint",
         "lua_ls",
-        "jsonls",
         "html",
         "tailwindcss",
         "dockerls",
         "gopls",
-        -- "hadolint",
-        "dockerls",
         "jsonls",
-        -- "prettier",
+        "kotlin_language_server",
+        "ts_ls",
       },
       handlers = {
         lsp.default_setup,
         lua_ls = function()
           local lua_opts = lsp.nvim_lua_ls()
           require("lspconfig").lua_ls.setup(lua_opts)
+        end,
+        eslint = function()
+          require("lspconfig").eslint.setup({
+            settings = {
+              rulesCustomizations = {
+                ["no-console"] = "warn", -- Aviso para console.log
+                ["indent"] = { "error", 2 }, -- Identação de 2 espaços
+                ["quotes"] = { "error", "double" }, -- Usar aspas duplas
+                ["semi"] = { "error", "always" }, -- Exigir ponto e vírgula
+                ["no-var"] = "error", -- Proíbe o uso de var
+                ["prefer-const"] = "error", -- Incentiva o uso de const
+                ["no-unused-vars"] = "error", -- Erro para variáveis não utilizadas
+                ["sort-imports"] = {
+                  "error",
+                  {
+                    ignoreCase = true,
+                    ignoreDeclarationSort = true,
+                    allowSeparatedGroups = false,
+                  },
+                },
+              },
+            },
+          })
         end,
       },
     })
@@ -68,11 +90,13 @@ return {
         end,
       },
       sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip", keyword_length = 2 },
-        { name = "buffer", keyword_length = 3 },
-        { name = "path" },
+        { name = "tabnine", priority = 1000 },
+        { name = "nvim_lsp", priority = 900 },
+        { name = "luasnip", priority = 800, keyword_length = 2 },
+        { name = "buffer", priority = 700, keyword_length = 3 },
+        { name = "path", priority = 600 },
       },
+
       mapping = cmp.mapping.preset.insert({
         ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
         ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
