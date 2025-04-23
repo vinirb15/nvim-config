dc389b48b7007d4b11f37077b4be7ff82a90f224
vbueno <viniciusbueno15@gmail.com>
2025-04-23 17:12:29 -0300

    (feat): Add indent-rainbow plugin to Neovim configuration
    - Integrate indent-blankline.nvim with indent-rainbowline
    - Enhance code readability with colorful indentation

---
+++ lua/plugins/indent-rainbow.lua
@@ -0,0 +1,10 @@
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	opts = function(_, opts)
+		return require("indent-rainbowline").make_opts(opts)
+	end,
+	dependencies = {
+		"TheGLander/indent-rainbowline.nvim",
+	},
+}
