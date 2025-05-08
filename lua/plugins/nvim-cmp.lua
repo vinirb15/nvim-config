468441191cb4b0a2735f85bf4f055f1af0acbd3f
vbueno <viniciusbueno15@gmail.com>
2025-05-08 00:46:18 -0300

    (refac): simplify nvim-cmp configuration   - remove buffer source from dependencies   - remove lsp-zero cmp_action dependency   - disable tabnine and nvim_lsp sources   - remove custom key mappings

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -2,14 +2,11 @@ return {
   "hrsh7th/nvim-cmp",
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline"
   },
   config = function()
     local cmp = require("cmp")
-    local cmp_action = require("lsp-zero").cmp_action()
-    local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
     -- `/` cmdline setup.
     cmp.setup.cmdline("/", {
@@ -36,22 +33,11 @@ return {
 
     cmp.setup({
       sources = {
-        { name = "tabnine", priority = 1000 },
-        { name = 'nvim_lsp' },
+        -- { name = "tabnine", priority = 1000 },
+        -- { name = 'nvim_lsp' },
         { name = 'buffer' },
         { name = 'path' },
-      },
-
-      mapping = cmp.mapping.preset.insert({
-        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.confirm(),
-        ["<C-f>"] = cmp_action.luasnip_jump_forward(),
-        ["<C-b>"] = cmp_action.luasnip_jump_backward(),
-        -- ["<Tab>"] = cmp_action.luasnip_supertab(),
-        -- ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
-      }),
+      }
     })
   end,
 }
