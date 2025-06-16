25f50291aebae706d0825be247e7aaf7edbacf84
vbueno <vinicius.bueno@compass.uol>
2025-06-16 16:39:34 -0300

    (feat): add gemini.nvim plugin configuration

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,25 +1,29 @@
 return {
--- {
---    "github/copilot.vim",
---    lazy = false,
---    config = function()
---      vim.g.copilot_no_tab_map = true
---      vim.api.nvim_set_keymap("i", "<C-e>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
---    end,
---  },
--- 	{
--- 	"codota/tabnine-nvim",
--- 	build = "./dl_binaries.sh",
--- 	config = function()
--- 	require("tabnine").setup({
--- 		disable_auto_comment = true,
--- 		accept_keymap = "<C-e>",
--- 		dismiss_keymap = "<C-]>",
--- 		debounce_ms = 800,
--- 		suggestion_color = { gui = "#808080", cterm = 244 },
--- 		exclude_filetypes = { "TelescopePrompt" },
--- 		log_file_path = nil,
--- 	})
--- 	end,
--- },
+	--  {
+	--    "github/copilot.vim",
+	--    lazy = false,
+	--    config = function()
+	--      vim.g.copilot_no_tab_map = true
+	--      vim.api.nvim_set_keymap("i", "<C-e>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+	--    end,
+	--  },
+	-- {
+	-- "codota/tabnine-nvim",
+	-- build = "./dl_binaries.sh",
+	-- config = function()
+	-- require("tabnine").setup({
+	-- 	disable_auto_comment = true,
+	-- 	accept_keymap = "<C-e>",
+	-- 	dismiss_keymap = "<C-]>",
+	-- 	debounce_ms = 800,
+	-- 	suggestion_color = { gui = "#808080", cterm = 244 },
+	-- 	exclude_filetypes = { "TelescopePrompt" },
+	-- 	log_file_path = nil,
+	-- })
+	-- end,
+	--  },
+	--  {
+	--    'kiddos/gemini.nvim',
+	--    opts = {}
+	--  }
 }
