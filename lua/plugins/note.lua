638d39928e2d90b04d7c1bf0c79ce09df97ea5ed
vbueno <viniciusbueno15@gmail.com>
2025-12-05 12:27:51 -0300

    (feat): add comprehensive note plugin keybindings with descriptions - added keybindings for daily note, index, search, report, and navigation - added task management keybindings for start and done actions

--- lua/plugins/note.lua
+++ lua/plugins/note.lua
@@ -8,13 +8,21 @@
     cmd = 'Note',
     ft = 'note',
     keys = {
-      {'<leader>N', -- [t]elescope [n]ote
+      {'<leader>Nn', '<cmd>Note<CR>', desc = 'Open daily note'},
+      {'<leader>Ni', '<cmd>NoteIndex<CR>', desc = 'Open note index'},
+      {'<leader>NS',
         function()
           require('telescope.builtin').live_grep({
             cwd = require('note.api').current_note_root()
           })
         end,
-        mode = 'n'
-      }
+        desc = 'Search notes'
+      },
+      {'<leader>Nr', '<cmd>NoteReport<CR>', desc = 'Note report', ft = 'note'},
+      {'<leader>Nc', '<cmd>NoteCurrentItem<CR>', desc = 'Jump to current item', ft = 'note'},
+      {'<leader>Nt', '<cmd>NoteTime<CR>', desc = 'Insert timestamp', ft = 'note'},
+      {'<leader>Nl', '<cmd>NoteGoLink<CR>', desc = 'Follow link', ft = 'note'},
+      {'<leader>Nd', '<cmd>NoteTaskDone<CR>', desc = 'Note task done', ft = 'note'},
+      {'<leader>Ns', '<cmd>NoteTaskStart<CR>', desc = 'Note task start', ft = 'note'},
     }
   }
